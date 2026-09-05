d379dbb5d391cc61f514d921e8b6445bba96860a
Oliver <oliver.mckinlay03@gmail.com>
2026-09-05 21:49:23 +1000

    Project setup

--- out/build/GCC 14.2.0 x86_64-w64-mingw32 (ucrt64)/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/olive/OneDrive/Documents/University/2026/Semester 2/ESI/Assistive Backpack Code
-# Build directory: C:/Users/olive/OneDrive/Documents/University/2026/Semester 2/ESI/Assistive Backpack Code/out/build/GCC 14.2.0 x86_64-w64-mingw32 (ucrt64)
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
