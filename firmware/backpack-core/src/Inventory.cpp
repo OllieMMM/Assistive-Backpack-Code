dbc28998e51ca69e6da1510205c9f9c4c6e72bfe
Oliver <oliver.mckinlay03@gmail.com>
2026-09-19 16:47:31 +1000

    Clean inventory manager build checkpoint

--- firmware/backpack-core/src/Inventory.cpp
+++ firmware/backpack-core/src/Inventory.cpp
@@ -1,1 +1,11 @@
 #include <Inventory.h>
+#include <Item.h>
+
+Inventory inv;
+
+int main_inventory()
+{
+    // Does not work (build with CMake) because the function shave not yet been implemented. 
+    inv.addItem(1234);
+    return 0;
+}
